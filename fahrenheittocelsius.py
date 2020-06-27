1ac79566deeda5f2b1abd341f3c994337ae7e1c7
Ryanmickle <66557304+Ryanmickle@users.noreply.github.com>
2020-06-27 12:11:17 +0100

    Create fahrenheittocelsius.py

---
+++ fahrenheittocelsius.py
@@ -0,0 +1,12 @@
+#conversion
+
+def fahrenheitToCelicus(a):
+    convertion = (a - 32) * 5/9 
+    return convertion
+
+def main():
+    inputValue = 100
+    result = fahrenheitToCelicus(inputValue)
+    print("Fahrenheit for {} is {:.2f}".format(inputValue, result))
+
+main()
